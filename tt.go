d5eaa4991c46bf57a54ba8ab67e52291e487fad4
Jason Mooberry <jason@vimeo.com>
2013-11-13 23:36:00 -0500

    for the homies

--- tt.go
+++ tt.go
@@ -4,63 +4,202 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+        "github.com/jasonmoo/bloom"
 	"github.com/jasonmoo/bloom/scalable"
 	"log"
 	"os"
+	"runtime"
 )
 
 var (
-	a   = flag.String("a", "", "input file a")
-	b   = flag.String("b", "", "input file b")
-	est = flag.Uint("est", 1<<20, "estimated size of set a")
+	intersection = flag.Bool("i", false, "calculate the intersection")
+	diff         = flag.Bool("d", false, "calculate the difference")
+	union        = flag.Bool("u", false, "calculate the union")
+
+	unique = flag.Bool("unique", false, "output the unique set of the values")
+
+	// global unique filter
+	ufilter = scalable.New(1 << 20)
 )
 
 func main() {
 
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	flag.Parse()
 
-	if *a == "" || *b == "" {
-		fmt.Println("Usage: tt -a <filea.txt> -b <fileb.txt>")
-		os.Exit(1)
+	file_paths := flag.Args()
+
+	if *union {
+		// init to 1mm per file, will grow to whatever
+		filter := scalable.New(uint(len(file_paths) << 20))
+
+		for _, file_path := range file_paths {
+			file, err := os.Open(file_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				token := scanner.Bytes()
+				if !filter.Check(token) {
+					if *unique {
+						if ufilter.Check(token) {
+							continue
+						}
+						ufilter.Add(token)
+					}
+					fmt.Println(token)
+				}
+			}
+			file.Close()
+		}
+
+		return
 	}
 
-	filter := scalable.New(*est)
+	// optimize muthafuckas
+	if len(file_paths) == 2 {
 
-	filea, err := os.Open(*a)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer filea.Close()
+		filter := scalable.New(1 << 20)
 
-	var ct_a, ct_b, ct_found uint64
+		filea, err := os.Open(file_paths[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer filea.Close()
 
-	scanner := bufio.NewScanner(filea)
-	for scanner.Scan() {
-		ct_a++
-		filter.Add(scanner.Bytes())
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+		scanner := bufio.NewScanner(filea)
+		for scanner.Scan() {
+			filter.Add(scanner.Bytes())
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
 
-	fileb, err := os.Open(*b)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fileb.Close()
+		fileb, err := os.Open(file_paths[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer fileb.Close()
 
-	scanner = bufio.NewScanner(fileb)
-	for scanner.Scan() {
-		ct_b++
-		if t := scanner.Bytes(); filter.Check(t) {
-			ct_found++
-			fmt.Printf("%s\n", t)
+		scanner = bufio.NewScanner(fileb)
+
+		switch {
+		case *intersection:
+			for scanner.Scan() {
+				if token := scanner.Bytes(); filter.Check(token) {
+					if *unique {
+						if ufilter.Check(token) {
+							continue
+						}
+						ufilter.Add(token)
+					}
+					fmt.Println(token)
+				}
+			}
+			if err := scanner.Err(); err != nil {
+				log.Fatal(err)
+			}
+		case *diff:
+			for scanner.Scan() {
+				if token := scanner.Bytes(); !filter.Check(token) {
+					if *unique {
+						if ufilter.Check(token) {
+							continue
+						}
+						ufilter.Add(token)
+					}
+					fmt.Println(token)
+				}
+			}
+			if err := scanner.Err(); err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+
+	// init with 1mm per file, will grow to needed size
+	filters, filter_chan := make([]bloom.Bloom, len(file_paths)), make(chan bloom.Bloom, len(file_paths))
+
+	// may require throttling due to disk thrashing
+	for _, file_path := range file_paths {
+		go func() {
+			filter := scalable.New(1 << 20)
+			file, err := os.Open(file_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				filter.Add(scanner.Bytes())
+			}
+			filter_chan <- filter
+		}()
 	}
 
-	fmt.Fprintf(os.Stderr, "\nTotal A: %d\nTotal B: %d\nB found in A: %d\n", ct_a, ct_b, ct_found)
+	// fill the filters
+	for i := range filters {
+		filters[i] = <- filter_chan
+	}
+
+	// do the work
+	switch {
+	case *intersection:
+		for _, file_path := range file_paths {
+			file, err := os.Open(file_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			scanner := bufio.NewScanner(file)
+
+			NEXT_TOKEN:
+			for scanner.Scan() {
+				token := scanner.Bytes()
+				for _, filter := range filters {
+					if !filter.Check(token) {
+						goto NEXT_TOKEN
+					}
+				}
+				if *unique {
+					if ufilter.Check(token) {
+						continue
+					}
+					ufilter.Add(token)
+				}
+				fmt.Println(token)
+			}
+			file.Close()
+		}
+	case *diff:
+		for _, file_path := range file_paths {
+			file, err := os.Open(file_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			scanner := bufio.NewScanner(file)
+
+			for scanner.Scan() {
+				token := scanner.Bytes()
+				for _, filter := range filters {
+					if !filter.Check(token) {
+						if *unique {
+							if ufilter.Check(token) {
+								continue
+							}
+							ufilter.Add(token)
+						}
+						fmt.Println(token)
+					}
+				}
+			}
+			file.Close()
+		}
+	default:
+		fmt.Println("Usage: tt -[i,d,u] file1 file2[ file3..]")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 }
