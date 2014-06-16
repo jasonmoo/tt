ef8e227300ab31ba1eb211230a0e33f44e7169c2
Jason Mooberry <jasonmoo@me.com>
2014-06-15 22:45:02 -0700

    mappy

--- tt.go
+++ tt.go
@@ -4,13 +4,6 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"github.com/reducedb/bloom"
-	"github.com/reducedb/bloom/scalable"
-
-	// "crypto/sha1"
-	// "github.com/spaolacci/murmur3"
-	// "github.com/zhenjl/cityhash"
-
 	"log"
 	"os"
 	"time"
@@ -21,13 +14,12 @@ var (
 	diff         = flag.Bool("d", false, "calculate the difference")
 	union        = flag.Bool("u", false, "calculate the union")
 
-	blooms = flag.Uint("blooms", 1, "number of bloom filters to use")
+	// activate lossy processing
+	blooms     = flag.Uint("blooms", 0, "number of bloom filters to use (lossy)")
+	mash_bytes = flag.Bool("mash", false, "mash bytes for fewer collisions in bloom filters")
 
 	// buffered io
 	stdout = bufio.NewWriterSize(os.Stdout, 4096)
-
-	// unique filter
-	unique_set bloom.Bloom
 
 	// total tokens in output
 	total uint64
@@ -46,180 +38,160 @@ func main() {
 
 	flag.Parse()
 
-	file_paths := flag.Args()
-
-	unique_set = NewScalableBloom(*blooms)
-
-	// may omit entries due to false positives
-	// todo(jason): try crypto hash or use dual filters
-	if *union {
-
-		for _, file_path := range file_paths {
-
-			file, err := os.Open(file_path)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			scanner := bufio.NewScanner(file)
-
-			for scanner.Scan() {
-				token := scanner.Bytes()
-				if !unique_set.Check(token) {
-					stdout.Write(token)
-					stdout.WriteByte('\n')
-					total++
-					unique_set.Add(token)
-				}
-			}
-
-			file.Close()
-
-		}
-
-		return
-	}
-
-	// multi file handling below
-	sets := make([]bloom.Bloom, len(file_paths))
-
-	// may require throttling due to disk thrashing
-	// initial scan to fill the bloom filters
-	for i, file_path := range file_paths {
-
-		set := NewScalableBloom(*blooms)
-
-		file, err := os.Open(file_path)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		scanner := bufio.NewScanner(file)
-
-		for scanner.Scan() {
-			set.Add(scanner.Bytes())
-		}
-
-		file.Close()
-
-		sets[i] = set
-
-	}
-
-	// do the work
-	switch {
-
-	// unique set of tokens that exist in all files
-	case *intersection:
-		for _, file_path := range file_paths {
-
-			file, err := os.Open(file_path)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			scanner := bufio.NewScanner(file)
-
-		NEXT_TOKEN:
-			for scanner.Scan() {
-				token := scanner.Bytes()
-				for _, set := range sets {
-					if !set.Check(token) || unique_set.Check(token) {
-						goto NEXT_TOKEN
-					}
-				}
-				stdout.Write(token)
-				stdout.WriteByte('\n')
-				total++
-				unique_set.Add(token)
-			}
-
-			file.Close()
-
-		}
-
-	// unique set of tokens not in the intersection
-	case *diff:
-		for _, file_path := range file_paths {
-
-			file, err := os.Open(file_path)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			scanner := bufio.NewScanner(file)
-
-			for scanner.Scan() {
-				token := scanner.Bytes()
-				for _, set := range sets {
-					if !set.Check(token) && !unique_set.Check(token) {
-						stdout.Write(token)
-						stdout.WriteByte('\n')
-						total++
-						unique_set.Add(token)
-					}
-				}
-			}
-
-			file.Close()
-
-		}
-	default:
-		fmt.Println("Usage: tt -[i,d,u] file1 file2[ file3..]")
+	if !*intersection && !*diff && !*union {
+		fmt.Println("Usage: tt -[i,d,u] [-blooms N [-mash]] file1 file2[ file3..]")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-}
+	file_paths := flag.Args()
 
-type (
-	Bloomer struct {
-		bloom.Bloom
-		filters []bloom.Bloom
-	}
-)
+	switch {
 
-func NewScalableBloom(size uint) bloom.Bloom {
+	// activate bloom filter solution
+	case *blooms > 0:
 
-	filters := make([]bloom.Bloom, size)
+	// defaults to map solution
+	default:
 
-	for i, _ := range filters {
-		filter := scalable.New(4096)
-		// filter.SetHasher(adler32.New())
-		filter.Reset()
-		filters[i] = filter
-	}
+		if *union {
 
-	return &Bloomer{
-		filters: filters,
-	}
+			unique_set := make(map[string]bool)
 
-}
+			for _, file_path := range file_paths {
 
-func (b *Bloomer) Add(token []byte) bloom.Bloom {
-	token = append(make([]byte, len(token)), token...)
-	for _, filter := range b.filters {
-		filter.Add(token)
-		mash(token)
-	}
-	return b
-}
+				file, err := os.Open(file_path)
+				if err != nil {
+					log.Fatal(err)
+				}
 
-func (b *Bloomer) Check(token []byte) bool {
-	token = append(make([]byte, len(token)), token...)
-	for _, filter := range b.filters {
-		if !filter.Check(token) {
-			return false
+				scanner := bufio.NewScanner(file)
+
+				for scanner.Scan() {
+					token := scanner.Text()
+					if _, exists := unique_set[token]; !exists {
+						stdout.WriteString(token)
+						stdout.WriteByte('\n')
+						total++
+						unique_set[token] = true
+					}
+				}
+
+				file.Close()
+
+			}
+
+			return
 		}
-		mash(token)
-	}
-	return true
-}
 
-// modifies the underlying structure
-func mash(token []byte) {
-	for i, c := range token {
-		c ^= (20 * c)
-		token[i] = c
+		// multi file handling below
+		sets := make([]map[string]bool, len(file_paths))
+
+		// may require throttling due to disk thrashing
+		// initial scan to fill the bloom filters
+		for i, file_path := range file_paths {
+
+			set := make(map[string]bool)
+
+			file, err := os.Open(file_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			scanner := bufio.NewScanner(file)
+
+			for scanner.Scan() {
+				set[scanner.Text()] = true
+			}
+
+			file.Close()
+
+			sets[i] = set
+
+		}
+
+		// do the work
+		switch {
+
+		// unique set of tokens that exist in all files
+		case *intersection:
+
+			echoed_set := make(map[string]bool)
+
+			for _, file_path := range file_paths {
+
+				file, err := os.Open(file_path)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				scanner := bufio.NewScanner(file)
+
+			NEXT_TOKEN:
+				for scanner.Scan() {
+
+					token := scanner.Text()
+
+					if _, echoed := echoed_set[token]; echoed {
+						goto NEXT_TOKEN
+					}
+
+					for _, set := range sets {
+						if _, in_this_set := set[token]; !in_this_set {
+							goto NEXT_TOKEN
+						}
+					}
+
+					stdout.WriteString(token)
+					stdout.WriteByte('\n')
+					total++
+					echoed_set[token] = true
+
+				}
+
+				file.Close()
+
+			}
+
+		// unique set of tokens not in the intersection
+		case *diff:
+
+			echoed_set := make(map[string]bool)
+
+			for _, file_path := range file_paths {
+
+				file, err := os.Open(file_path)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				scanner := bufio.NewScanner(file)
+
+				for scanner.Scan() {
+
+					token := scanner.Text()
+
+					if _, echoed := echoed_set[token]; echoed {
+						continue
+					}
+
+					for _, set := range sets {
+						if _, in_this_set := set[token]; !in_this_set {
+							stdout.WriteString(token)
+							stdout.WriteByte('\n')
+							total++
+							echoed_set[token] = true
+							break
+						}
+					}
+
+				}
+
+				file.Close()
+
+			}
+		}
+
 	}
+
 }
