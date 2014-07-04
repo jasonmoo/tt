78fff51b25eef3aef64a130230195f75718a4dfa
Jason Mooberry <jasonmoo@me.com>
2014-07-04 10:06:52 -0700

    adding line counter

--- tt.go
+++ tt.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"regexp"
@@ -15,11 +16,6 @@ import (
 )
 
 type (
-	Bloomer struct {
-		bloom.Bloom
-		filters []bloom.Bloom
-	}
-
 	Emitter struct {
 		file    *os.File
 		scanner *bufio.Scanner
@@ -29,11 +25,6 @@ type (
 
 		current []byte
 	}
-
-	TokenStore interface {
-		Add([]byte)
-		Check([]byte) bool
-	}
 )
 
 var (
@@ -42,17 +33,20 @@ var (
 	union        = flag.Bool("u", false, "calculate the union")
 
 	// activate lossy processing
-	blooms = flag.Uint("blooms", 0, "number of bloom filters to use (lossy)")
+	estimated_lines = flag.Uint64("estimated_lines", 0, "use bloom filters above this number of lines to scan")
+	bloom_threshold = flag.String("bloom_threshold", "1M", "use bloom filters above this number of lines to scan")
 
 	// options
 	trim          = flag.Bool("trim", false, "trim each line")
 	regex_match   = flag.String("match", "", "only process matching lines")
 	regex_capture = flag.String("capture", "", "only process captured data")
 
-	buffer_size = flag.Int("buffer_size", 4096, "buffered io chunk size")
+	buffer_size = flag.Int("buffer_size", 1<<20, "buffered io chunk size")
 
-	// total tokens in output
-	total uint64
+	// totals
+	total_tokens_emitted  uint64
+	total_bytes_processed uint64
+	total_lines_scanned   uint64
 )
 
 func main() {
@@ -67,342 +61,345 @@ func main() {
 	defer func() {
 		stdout.Flush()
 		fmt.Fprintln(os.Stderr, "** Token Report **")
-		fmt.Fprintln(os.Stderr, "Tokens output: ", total)
-		fmt.Fprintln(os.Stderr, "Total time: ", time.Since(start))
+		fmt.Fprintln(os.Stderr, "Lines scanned: ", total_lines_scanned)
+		fmt.Fprintln(os.Stderr, "Tokens emitted: ", total_tokens_emitted)
+		fmt.Fprintln(os.Stderr, "Time: ", time.Since(start))
 	}()
 
 	if !*intersection && !*diff && !*union {
-		fmt.Println("Usage: tt -[i,d,u] [-match \"pattern\"] -[capture \"pattern\"] [-blooms N] file1 file2[ file3..]")
+		fmt.Println("Usage: tt -[i,d,u] [-match \"pattern\"] -[capture \"pattern\"] file1 file2[ file3..]")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	file_paths := flag.Args()
 
-	switch {
+	fmt.Fprintln(os.Stderr, "tt starting up")
 
-	// activate bloom filter solution
-	case *blooms > 0:
+	// if no estimate supplied, count lines
+	if *estimated_lines == 0 {
 
-		if *union {
+		for _, file_path := range file_paths {
 
-			unique_set := NewScalableBloom(*blooms)
-
-			for _, file_path := range file_paths {
-
-				e, err := NewEmitter(file_path, *regex_match, *regex_capture)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				for e.Scan() {
-					token := e.Bytes()
-					if !unique_set.Check(token) {
-						stdout.Write(token)
-						stdout.WriteByte('\n')
-						total++
-						unique_set.Add(token)
-					}
-				}
-
-				e.Close()
-
-			}
-
-			return
-		}
-
-		// multi file handling below
-		sets := make([]bloom.Bloom, len(file_paths))
-
-		// may require throttling due to disk thrashing
-		// initial scan to fill the bloom filters
-		for i, file_path := range file_paths {
-
-			set := NewScalableBloom(*blooms)
-
-			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+			file, err := os.Open(file_path)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			for e.Scan() {
-				set.Add(e.Bytes())
-			}
+			var lines uint64
 
-			e.Close()
+			buf := make([]byte, *buffer_size)
 
-			sets[i] = set
-
-		}
-
-		// do the work
-		switch {
-
-		// unique set of tokens that exist in all files
-		case *intersection:
-
-			echoed_set := NewScalableBloom(*blooms)
-
-			for _, file_path := range file_paths {
-
-				e, err := NewEmitter(file_path, *regex_match, *regex_capture)
-				if err != nil {
+			for {
+				n, err := file.Read(buf)
+				if err != nil && err != io.EOF {
 					log.Fatal(err)
 				}
-
-			NEXT_TOKEN:
-				for e.Scan() {
-
-					token := e.Bytes()
-
-					if echoed_set.Check(token) {
-						goto NEXT_TOKEN
-					}
-
-					for _, set := range sets {
-						if !set.Check(token) {
-							goto NEXT_TOKEN
-						}
-					}
-
-					stdout.Write(token)
-					stdout.WriteByte('\n')
-					total++
-					echoed_set.Add(token)
-
+				if n == 0 {
+					break
 				}
-
-				e.Close()
-
+				for _, c := range buf[:n] {
+					if c == '\n' {
+						lines++
+					}
+				}
 			}
 
-		// unique set of tokens not in the intersection
-		case *diff:
+			*estimated_lines += lines
 
-			echoed_set := NewScalableBloom(*blooms)
-
-			for _, file_path := range file_paths {
-
-				e, err := NewEmitter(file_path, *regex_match, *regex_capture)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				for e.Scan() {
-
-					token := e.Bytes()
-
-					if echoed_set.Check(token) {
-						continue
-					}
-
-					for _, set := range sets {
-						if !set.Check(token) {
-							stdout.Write(token)
-							stdout.WriteByte('\n')
-							total++
-							echoed_set.Add(token)
-						}
-					}
-
-				}
-
-				e.Close()
-
-			}
-		}
-
-	// defaults to map solution
-	default:
-
-		if *union {
-
-			unique_set := make(map[string]bool)
-
-			for _, file_path := range file_paths {
-
-				e, err := NewEmitter(file_path, *regex_match, *regex_capture)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				for e.Scan() {
-					token := e.Text()
-					if _, exists := unique_set[token]; !exists {
-						stdout.WriteString(token)
-						stdout.WriteByte('\n')
-						total++
-						unique_set[token] = true
-					}
-				}
-
-				e.Close()
-
-			}
-
-			return
-		}
-
-		// multi file handling below
-		sets := make([]map[string]bool, len(file_paths))
-
-		// may require throttling due to disk thrashing
-		// initial scan to fill the bloom filters
-		for i, file_path := range file_paths {
-
-			set := make(map[string]bool)
-
-			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for e.Scan() {
-				set[e.Text()] = true
-			}
-
-			e.Close()
-
-			sets[i] = set
-
-		}
-
-		// do the work
-		switch {
-
-		// unique set of tokens that exist in all files
-		case *intersection:
-
-			echoed_set := make(map[string]bool)
-
-			for _, file_path := range file_paths {
-
-				e, err := NewEmitter(file_path, *regex_match, *regex_capture)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-			NEXT_TOKEN2:
-				for e.Scan() {
-
-					token := e.Text()
-
-					if _, echoed := echoed_set[token]; echoed {
-						goto NEXT_TOKEN2
-					}
-
-					for _, set := range sets {
-						if _, in_this_set := set[token]; !in_this_set {
-							goto NEXT_TOKEN2
-						}
-					}
-
-					stdout.WriteString(token)
-					stdout.WriteByte('\n')
-					total++
-					echoed_set[token] = true
-
-				}
-
-				e.Close()
-
-			}
-
-		// unique set of tokens not in the intersection
-		case *diff:
-
-			echoed_set := make(map[string]bool)
-
-			for _, file_path := range file_paths {
-
-				e, err := NewEmitter(file_path, *regex_match, *regex_capture)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				for e.Scan() {
-
-					token := e.Text()
-
-					if _, echoed := echoed_set[token]; echoed {
-						continue
-					}
-
-					for _, set := range sets {
-						if _, in_this_set := set[token]; !in_this_set {
-							stdout.WriteString(token)
-							stdout.WriteByte('\n')
-							total++
-							echoed_set[token] = true
-							break
-						}
-					}
-
-				}
-
-				e.Close()
-
-			}
+			file.Close()
 		}
 
 	}
+
+	// count number of bytes to process
+	for _, file_path := range file_paths {
+
+		info, err := os.Stat(file_path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total_bytes_processed += uint64(info.Size())
+
+	}
+
+	fmt.Fprintln(os.Stderr, "Bytes to process: ", total_bytes_processed)
+	fmt.Fprintln(os.Stderr, "Lines to process: ", *estimated_lines)
+
+	os.Exit(0)
+
+	// if *estimated_lines > *bloom_threshold {
+
+	// 	// double the lines processed
+	// 	bloom_size := *estimated_lines * 2
+
+	// 	if *union {
+
+	// 		unique_set := NewScalableBloom(*bloom_size)
+
+	// 		for _, file_path := range file_paths {
+
+	// 			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
+
+	// 			for e.Scan() {
+	// 				token := e.Bytes()
+	// 				if !unique_set.Check(token) {
+	// 					stdout.Write(token)
+	// 					stdout.WriteByte('\n')
+	// 					unique_set.Add(token)
+	// 				}
+	// 			}
+
+	// 			e.Close()
+
+	// 		}
+
+	// 		return
+	// 	}
+
+	// 	// multi file handling below
+	// 	sets := make([]bloom.Bloom, len(file_paths))
+
+	// 	// may require throttling due to disk thrashing
+	// 	// initial scan to fill the bloom filters
+	// 	for i, file_path := range file_paths {
+
+	// 		set := NewScalableBloom(*use_bloom)
+
+	// 		e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+
+	// 		for e.Scan() {
+	// 			set.Add(e.Bytes())
+	// 		}
+
+	// 		e.Close()
+
+	// 		sets[i] = set
+
+	// 	}
+
+	// 	// do the work
+	// 	switch {
+
+	// 	// unique set of tokens that exist in all files
+	// 	case *intersection:
+
+	// 		echoed_set := NewScalableBloom(*use_bloom)
+
+	// 		for _, file_path := range file_paths {
+
+	// 			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
+
+	// 		NEXT_TOKEN:
+	// 			for e.Scan() {
+
+	// 				token := e.Bytes()
+
+	// 				if echoed_set.Check(token) {
+	// 					goto NEXT_TOKEN
+	// 				}
+
+	// 				for _, set := range sets {
+	// 					if !set.Check(token) {
+	// 						goto NEXT_TOKEN
+	// 					}
+	// 				}
+
+	// 				stdout.Write(token)
+	// 				stdout.WriteByte('\n')
+	// 				echoed_set.Add(token)
+
+	// 			}
+
+	// 			e.Close()
+
+	// 		}
+
+	// 	// unique set of tokens not in the intersection
+	// 	case *diff:
+
+	// 		echoed_set := NewScalableBloom(*use_bloom)
+
+	// 		for _, file_path := range file_paths {
+
+	// 			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
+
+	// 			for e.Scan() {
+
+	// 				token := e.Bytes()
+
+	// 				if echoed_set.Check(token) {
+	// 					continue
+	// 				}
+
+	// 				for _, set := range sets {
+	// 					if !set.Check(token) {
+	// 						stdout.Write(token)
+	// 						stdout.WriteByte('\n')
+	// 						echoed_set.Add(token)
+	// 					}
+	// 				}
+
+	// 			}
+
+	// 			e.Close()
+
+	// 		}
+	// 	}
+
+	// 	// defaults to map solution
+	// } else {
+
+	// 	if *union {
+
+	// 		unique_set := make(map[string]bool)
+
+	// 		for _, file_path := range file_paths {
+
+	// 			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
+
+	// 			for e.Scan() {
+	// 				token := e.Text()
+	// 				if _, exists := unique_set[token]; !exists {
+	// 					stdout.WriteString(token)
+	// 					stdout.WriteByte('\n')
+
+	// 					unique_set[token] = true
+	// 				}
+	// 			}
+
+	// 			e.Close()
+
+	// 		}
+
+	// 		return
+	// 	}
+
+	// 	// multi file handling below
+	// 	sets := make([]map[string]bool, len(file_paths))
+
+	// 	// may require throttling due to disk thrashing
+	// 	// initial scan to fill the bloom filters
+	// 	for i, file_path := range file_paths {
+
+	// 		set := make(map[string]bool)
+
+	// 		e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+
+	// 		for e.Scan() {
+	// 			set[e.Text()] = true
+	// 		}
+
+	// 		e.Close()
+
+	// 		sets[i] = set
+
+	// 	}
+
+	// 	// do the work
+	// 	switch {
+
+	// 	// unique set of tokens that exist in all files
+	// 	case *intersection:
+
+	// 		echoed_set := make(map[string]bool)
+
+	// 		for _, file_path := range file_paths {
+
+	// 			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
+
+	// 		NEXT_TOKEN2:
+	// 			for e.Scan() {
+
+	// 				token := e.Text()
+
+	// 				if _, echoed := echoed_set[token]; echoed {
+	// 					goto NEXT_TOKEN2
+	// 				}
+
+	// 				for _, set := range sets {
+	// 					if _, in_this_set := set[token]; !in_this_set {
+	// 						goto NEXT_TOKEN2
+	// 					}
+	// 				}
+
+	// 				stdout.WriteString(token)
+	// 				stdout.WriteByte('\n')
+
+	// 				echoed_set[token] = true
+
+	// 			}
+
+	// 			e.Close()
+
+	// 		}
+
+	// 	// unique set of tokens not in the intersection
+	// 	case *diff:
+
+	// 		echoed_set := make(map[string]bool)
+
+	// 		for _, file_path := range file_paths {
+
+	// 			e, err := NewEmitter(file_path, *regex_match, *regex_capture)
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
+
+	// 			for e.Scan() {
+
+	// 				token := e.Text()
+
+	// 				if _, echoed := echoed_set[token]; echoed {
+	// 					continue
+	// 				}
+
+	// 				for _, set := range sets {
+	// 					if _, in_this_set := set[token]; !in_this_set {
+	// 						stdout.WriteString(token)
+	// 						stdout.WriteByte('\n')
+
+	// 						echoed_set[token] = true
+	// 						break
+	// 					}
+	// 				}
+
+	// 			}
+
+	// 			e.Close()
+
+	// 		}
+	// 	}
+
+	// }
 
 }
 
 func NewScalableBloom(size uint) bloom.Bloom {
-
-	filters := make([]bloom.Bloom, size)
-
-	for i, _ := range filters {
-		filter := scalable.New(4096)
-		// filter.SetHasher(adler32.New())
-		filter.Reset()
-		filters[i] = filter
-	}
-
-	return &Bloomer{
-		filters: filters,
-	}
-
-}
-
-func (b *Bloomer) Add(token []byte) bloom.Bloom {
-
-	if *blooms > 1 {
-		token = append(make([]byte, len(token)), token...)
-	}
-
-	for _, filter := range b.filters {
-		filter.Add(token)
-		if *blooms > 1 {
-			mash(token)
-		}
-	}
-
-	return b
-}
-
-func (b *Bloomer) Check(token []byte) bool {
-	if *blooms > 1 {
-		token = append(make([]byte, len(token)), token...)
-	}
-	for _, filter := range b.filters {
-		if !filter.Check(token) {
-			return false
-		}
-		if *blooms > 1 {
-			mash(token)
-		}
-	}
-	return true
-}
-
-// modifies the underlying structure
-func mash(token []byte) {
-	for i, c := range token {
-		token[i] ^= (20 * c)
-	}
+	// memory aligned double size in millions
+	return scalable.New(size << 21)
 }
 
 func NewEmitter(file_path, regex_match, regex_capture string) (*Emitter, error) {
@@ -431,6 +428,7 @@ func NewEmitter(file_path, regex_match, regex_capture string) (*Emitter, error) 
 }
 func (e *Emitter) Scan() bool {
 	for e.scanner.Scan() {
+		total_lines_scanned++
 		e.current = e.scanner.Bytes()
 		if *trim {
 			e.current = bytes.TrimSpace(e.current)
@@ -446,6 +444,7 @@ func (e *Emitter) Scan() bool {
 				continue
 			}
 		}
+		total_tokens_emitted++
 		return true
 	}
 	return false
